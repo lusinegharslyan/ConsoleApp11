b859b0fb18f1df5242b522f747f1e05fb4fdedad
lusinegharslyan <lusine.karslyan12@gmail.com>
2024-09-14 09:27:21 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+
+
+    class Cage<T>
+    {
+        public Cage(T t)
+        {
+            this.t = t;
+        }
+
+        public T t { get; set; }
+    }
+
+
+    class Animal
+    {
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+
+    class Dog:Animal
+    {
+        public Dog(string name, int age):base(name,age)
+        {
+         
+        }
+              
+    }
+
+    class Cat : Animal
+    {
+        public Cat(string name, int age):base(name, age)
+        {
+
+        }
+    }
+
+    class Cow : Animal
+    {
+        public Cow(string name, int age) : base(name, age)
+        {
+        }
+    }
+
+    class Pig : Animal
+    {
+        public Pig(string name, int age) : base(name, age)
+        {
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cage<Dog> dogCage = new Cage<Dog>(new Dog("Sevo", 5));
+            Cage<Cat> catCage = new Cage<Cat>(new Cat("Miki", 3));
+            
+
+        }
+    }
+}
